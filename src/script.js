6d59b9ab52988c7ee9c0d6e156489448e0aa5e0f
Jaku <mineface98@gmail.com>
2026-09-17 17:04:59 +0200

    commit przed deployem

--- src/script.js
+++ src/script.js
@@ -406,7 +406,7 @@ document.getElementById('contact-form')?.addEventListener('submit', async (e) =>
 
     try {
         // wysyłanie żądania POST do backendu
-        const response = await fetch('http://127.0.0.1:8000/api/contact/', {
+        const response = await fetch('https://api.jankulak.me/api/contact/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
